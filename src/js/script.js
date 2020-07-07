a178a48e8de9e132f4a90518cb1445ac364f2833
Dirk <dirkthomasmulder@gmail.com>
2020-07-07 14:02:28 +0700

    technical part done

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,87 @@
 $(document).ready(function () {
 
-    $('.we').bind('click mouseover', function () {
-        $('.we--modal').toggleClass('hidden');
-    })
+    // we function
+    function we() {
+        // when the page is loaded the JS checks the viewport. According to that, it will output either mobile or desktop JS code. 
+        if ($(window).width() <= 767) {
+            $('.we').click(function () {
+                $('.we--modal').toggleClass('hidden');
+            })
+            $('#we-close').click(function () {
+                $('.we--modal').addClass('hidden');
+            })
+        } else {
+            $('.we').bind('mouseover', function () {
+                $('.we--modal').toggleClass('hidden');
+            })
 
-    $('.we').mouseout(function () {
-        $('.we--modal').addClass('hidden');
-    })
+            $('.we').mouseout(function () {
+                $('.we--modal').addClass('hidden');
+            })
+        }
+    }
 
-    $('.help').bind('click mouseover', function () {
-        $('.help--modal').toggleClass('hidden');
-    })
+    // help function
+    function help() {
+        if ($(window).width() <= 767) {
+            $('.help').click(function () {
+                $('.help--modal').toggleClass('hidden');
+            })
+            $('#help-close').click(function () {
+                $('.help--modal').addClass('hidden');
+            })
+        } else {
+            $('.help').bind('mouseover', function () {
+                $('.help--modal').toggleClass('hidden');
+            })
 
-    $('.help').mouseout(function () {
-        $('.help--modal').addClass('hidden');
-    })
+            $('.help').mouseout(function () {
+                $('.help--modal').addClass('hidden');
+            })
+        }
+    }
 
-    $('.you').on('click mouseover', function () {
-        $('.you--modal').removeClass('hidden');
-    })
+    //you function
+    function you() {
+        if ($(window).width() <= 767) {
+            $('.you').click(function () {
+                $('.you--modal').toggleClass('hidden');
+            })
+            $('#you-close').click(function () {
+                $('.you--modal').addClass('hidden');
+            })
+        } else {
+            $('.you').on('mouseover', function () {
+                $('.you--modal').removeClass('hidden');
+            })
 
-    $('.you').mouseout(function () {
-        $('.you--modal').addClass('hidden');
-    })
+            $('.you').mouseout(function () {
+                $('.you--modal').addClass('hidden');
+            })
+        }
+    }
+
+    // make the contact FAB work
+    function contact() {
+        $('.modal--fab').hide();
+
+        $('.fab--inner').click(function () {
+            $('.fab--inner').toggleClass('rotate-45');
+            $('.fab').toggleClass('fab-big');
+            $('.modal--fab').delay(200).fadeToggle();
+            $('header > svg').toggleClass('white_logo');
+        })
+    }
+
+
+    we();
+    help();
+    you();
+    contact();
+
+    // contact
+
+
+
 
 });
